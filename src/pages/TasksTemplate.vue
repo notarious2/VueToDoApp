f5eb5d7291116b00754e6080ee64e8cb3d4ce52f
notarious1~git config --global user.email notarious2@gmail.comgit config --global user.name notarious <notarious2@gmail.com>
2022-10-10 15:40:07 +0500

    major style and organization changes in the to-do template

--- src/pages/TasksTemplate.vue
+++ src/pages/TasksTemplate.vue
@@ -3,10 +3,17 @@
     <header>HEADER GOES HERE</header>
     <div class="grid-item-todo">
       <h1>{{ date }}</h1>
-      <draggable :list="myArray" item-key="id" @change="updateList">
+      <div v-if="!display" class="no-tasks">No Tasks to Display</div>
+      <draggable v-else :list="myArray" item-key="id" @change="updateList">
         <template #item="{ element }">
           <div class="flexbox">
-            <div class="flex-text">
+            <div class="flex-id">
+              <p>{{ element.id }}</p>
+            </div>
+            <div
+              class="flex-text"
+              :class="{ editSelectedBorder: element.editable }"
+            >
               <p
                 :contenteditable="element.editable"
                 @input="editText"
@@ -22,10 +29,16 @@
                 :class="{ editSelected: element.editable }"
                 @click="makeEditable(element)"
               />
-              <a class="close" @click="deleteTask(element)">x</a>
-              <input
-                type="checkbox"
-                class="check"
+              <font-awesome-icon
+                @click="deleteTask(element)"
+                icon="fas
+              fa-trash"
+                class="delete-icon"
+              />
+              <font-awesome-icon
+                icon="fas fa-check"
+                class="check-icon"
+                :class="{ isChecked: element.completed }"
                 :id="element.id"
                 v-model="element.completed"
                 @click="checkUncheck(element)"
@@ -34,15 +47,17 @@
           </div>
         </template>
       </draggable>
-      <form @submit.prevent="addTask">
-        <label>text </label>
-        <input v-model="task" type="text" />
-        <button>add task</button>
+      <form class="form-control" @submit.prevent="addTask">
+        <input class="task-input" v-model="task" type="text" />
+        <button class="button-74">add task</button>
       </form>
     </div>
     <div class="grid-item-calendar">
-      <h1>My Calendar</h1>
-      <FullCalendar :options="calendarOptions" />
+      <FullCalendar
+        :options="calendarOptions"
+        ref="calendar"
+        class="calendar"
+      />
     </div>
     <footer>
       <p>FOOTER GOES HERE</p>
@@ -51,44 +66,86 @@
 </template>
 
 <script setup>
-import { reactive, ref } from "vue";
+import { reactive, ref, watch } from "vue";
 import draggable from "vuedraggable";
+import { formatDate } from "@fullcalendar/core";
 
 import FullCalendar from "@fullcalendar/vue3";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
+const today = formatDate(new Date(), {
+  month: "long",
+  year: "numeric",
+  day: "numeric",
+});
+
 const task = ref();
 
-const date = ref("1/1/2022");
+const date = ref(today);
+console.log(date.value);
 
 const editedText = ref("");
 
 const myArray = reactive([
   { text: "AAA", id: 1, completed: true, editable: false },
   { text: "BBB", id: 2, completed: false, editable: false },
-  { text: "CCC", id: 3, completed: false, editable: false },
+  { text: "CCC", id: 3, completed: true, editable: false },
   { text: "DDD", id: 4, completed: true, editable: false },
 ]);
+
+// Ref for calendar in the DOM
+const calendar = ref();
+
+// Count the number of completed tasks
+
+const notCompletedTasks = ref(myArray.filter((ob) => !ob.completed).length);
+console.log(notCompletedTasks.value);
+
+// To display if there are tasks
+const display = myArray.length > 0 ? ref(true) : ref(false);
 
 const calendarOptions = {
   plugins: [dayGridPlugin, interactionPlugin],
   initialView: "dayGridMonth",
   dateClick: handleDateClick,
-  events: [
-    { title: "3 tasks", date: "2022-10-06" },
-    { title: "event 2", date: "2019-04-02" },
-  ],
-  eventColor: "transparent",
-  eventTextColor: "red",
+  // events: [
+  //   { title: "not done: " + notCompletedTasks.value, date: "2022-10-06" },
+  //   { title: "not done: 3", date: "2022-10-07" },
+  // ],
+  // eventColor: "transparent",
+  // eventTextColor: "red",
+  height: "auto",
   selectable: true,
 };
 
+// onMounted(() => {
+//   let calendarApi = ref(calendar.value.getApi());
+//   calendarApi.value.refetchEvents();
+// });
+
+// WATCHING TO-DO LIST TO DISPLAY/NOT DISPLAY
+watch(myArray, function () {
+  if (myArray.length > 0) {
+    display.value = true;
+  } else {
+    display.value = false;
+  }
+});
+
+// WATCHING TO-DO LIST TO DISPLAY # UNCOMPLETED TASKS
+watch(myArray, function () {
+  notCompletedTasks.value = myArray.filter((ob) => ob.completed).length;
+});
 // WORKING WITH CALENDAR
 
 function handleDateClick(arg) {
-  alert("date click!" + arg.dateStr);
-  date.value = arg.dateStr;
+  // alert("date click!" + arg.dateStr);
+  date.value = formatDate(arg.date, {
+    month: "long",
+    year: "numeric",
+    day: "numeric",
+  });
 }
 
 // listen to input inside edited paragraph text
@@ -146,8 +203,36 @@ function deleteTask(element) {
 </script>
 
 <style scoped>
+* {
+  font-family: "Kalam", cursive;
+}
 h1 {
   text-align: center;
+}
+
+.form-control {
+  margin-top: 20px;
+  display: flex;
+  height: 40px;
+  justify-content: space-between;
+}
+
+.task-input {
+  width: 80%;
+  margin-left: 30px;
+  margin-right: 10px;
+  line-height: 25px;
+  font-size: 18px;
+  background-color: #fbeee0;
+  border: 2px solid #422800;
+}
+
+.no-tasks {
+  height: 100px;
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  font-weight: bold;
 }
 
 /* GENERAL PAGE LAYOUT WITH GRIDS */
@@ -170,6 +255,10 @@ header {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.calendar {
+  margin-top: 10px;
 }
 
 footer {
@@ -209,30 +298,65 @@ footer {
   footer {
     grid-row: 4/5;
   }
+  .form-control {
+    flex-direction: column;
+    align-items: center;
+  }
+  .button-74 {
+    width: 90px;
+    flex-shrink: 0;
+    margin-top: 10px;
+    margin-right: 40px;
+  }
 }
 /* TO-DO INNER LAYOUT WITH FLEXBOX */
 
 .flexbox {
   display: flex;
   justify-content: space-between;
+  margin-left: 10px;
+  margin-right: 10px;
+  gap: 5px;
 }
 .flex-text {
   background: lightgray;
   flex: 1;
-  border: 1px solid orange;
 }
 
+.flex-id {
+  flex-basis: 15px;
+}
+.flex-id p {
+  font-weight: bold;
+}
+
+/* Change border of the text if edit is selected*/
+
+.editSelectedBorder {
+  border: 1px solid orange;
+}
 .flex-buttons {
   background: lightyellow;
   flex-basis: 70px;
   display: flex;
-  justify-content: space-evenly;
   align-items: center;
+  gap: 10px;
+  padding: 5px;
 }
 
 .edit-icon {
-  margin-top: 10px;
   font-size: 18px;
+}
+
+.delete-icon:hover {
+  color: #9b4848;
+}
+
+/* CHECKBOX */
+.check-icon {
+  margin-left: 15px;
+  color: #b04b4b;
+  font-size: 26px;
 }
 
 /* Make pen green if edit is selected*/
@@ -240,4 +364,46 @@ footer {
 .editSelected {
   color: green;
 }
+
+.isChecked {
+  color: green;
+}
+
+/* BUTTON */
+.button-74 {
+  background-color: #fbeee0;
+  border: 2px solid #422800;
+  border-radius: 25px;
+  box-shadow: #422800 3px 3px 0 0;
+  color: #422800;
+  cursor: pointer;
+  font-weight: 300;
+  font-size: 16px;
+  padding: 0 18px;
+  line-height: 20px;
+  text-align: center;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  flex: 0;
+  margin-right: 15px;
+  flex-basis: 100px;
+}
+
+.button-74:hover {
+  background-color: #fff;
+}
+
+.button-74:active {
+  box-shadow: #422800 2px 2px 0 0;
+  transform: translate(2px, 2px);
+}
+
+/* @media (min-width: 768px) {
+  .button-74 {
+    min-width: 120px;
+    padding: 0 25px;
+  }
+} */
 </style>
