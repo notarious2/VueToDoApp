954a221bb4360a67d1df7b2907a10a6eeca595f0
notarious1~git config --global user.email notarious2@gmail.comgit config --global user.name notarious <notarious2@gmail.com>
2022-10-30 17:08:00 +0500

    added deploy file for gh pages

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+cp index.html 404.html
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:notarious2/VueToDoApp.git master:gh-pages
+
+cd -
